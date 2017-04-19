8a8596ef054aa04b869e1379e17f8f16a52da927
Swizzlr <git@swizzlr.co>
2017-04-19 15:09:32 +0100

    Tada

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
     name: "TitanLoggingSwiftyBeaver",
     dependencies: [
       .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1, minor: 2),
-      .Package(url: "https://github.com/BermudaDigitalStudio/TitanCore.git", majorVersion: 0, minor: 3)
+      .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 3)
     ]
 )

471a526c3c04666936f708ff5d3ca660f8f39bcc
Ryan Collins <ryan@ryanmcollins.com>
2017-09-20 06:59:09 -0400

    Bump SwiftyBeaver Version for Swift 4

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "TitanLoggingSwiftyBeaver",
     dependencies: [
-      .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1, minor: 2),
+      .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1, minor: 4),
       .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 3)
     ]
 )

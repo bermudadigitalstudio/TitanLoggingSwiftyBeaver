45e2c1b057ffc875f99378e4450b1266c789db00
Thomas Catterall <me@swizzlr.co>
2017-11-15 09:46:38 -0500

    Support Titan 0.8.0

--- Package.swift
+++ Package.swift
@@ -1,11 +1,24 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "TitanLoggingSwiftyBeaver",
+     products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "TitanLoggingSwiftyBeaver",
+            targets: ["TitanLoggingSwiftyBeaver"]),
+    ],
     dependencies: [
-      .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1, minor: 4),
-      .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 3)
+      .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.4.0"),
+      .package(url: "https://github.com/bermudadigitalstudio/Titan.git", from: "0.8.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "TitanLoggingSwiftyBeaver",
+            dependencies: ["SwiftyBeaver", "TitanCore"]),
     ]
 )
